56286202895f5cf016a4107d22e438c71741e93d
Lauv_Robert <lauvr027@hsd.k12.or.us>
2017-05-11 10:43:29 -0700

    adding successful code

--- BMICalculator.py
+++ BMICalculator.py
@@ -4,9 +4,15 @@
 
 # Define Function below
 # be sure to return an integer
+def calculateBMI (height, weight):
+    total = (weight / (height)**2) * 703
+    total = round(total, 0)
+    return (total)
+    
 
 
 if __name__ == '__main__':
     # Call the function in here if you want to test it
     # Make sure it's indented
-    pass # remove or comment out this line if you wish to test the function 
+    pass # remove or comment out this line if you wish to test the function
+
